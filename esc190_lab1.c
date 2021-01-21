0e9d5c9dd6466f5ace0af4b1f759f1fd22ea3ae3
Jenna Gu <jennagjr@outlook.com>
2021-01-21 15:54:34 -0500

    deleted comments

--- esc190_lab1.c
+++ esc190_lab1.c
@@ -7,14 +7,6 @@ void f(int *p_a){
 
 // problem 2
 void insertion_sort(int arr[], int length){
-    /*
-    printf("sizeof(a): %d\n", sizeof arr);
-    printf("sizeof(a[0]): %d\n", sizeof arr[0]);
-    
-    int size = sizeof arr / sizeof arr[0];
-    printf("size: %d\n", size);
-    */
-
     for (int i = 1; i < length; i++){
         while(arr[i] < arr[i-1]){
             int temp = arr[i];
@@ -26,16 +18,13 @@ void insertion_sort(int arr[], int length){
             }
         }
     }
-
-    // https://www.geeksforgeeks.org/insertion-sort/
-    // website for insertion-sort c algorithm
 }
 
 void print_array(int arr[]){
     int i = 0;
     while(i < 6){
        printf("%d\n", arr[i]);
-       i++; // same as i = i + 1
+       i++;
    }
 }
 
@@ -49,9 +38,6 @@ int main(){
     //problem 2
 
     int arr[] = {12,2,31,11,5,9};
-    //printf("%d\n", sizeof(arr));
-    //printf("%d\n", sizeof arr[0]);
-
     //int a[5] = {5, 3, 7, 5, 1};
     int size = sizeof arr / sizeof arr[0];
     insertion_sort(arr, size);
