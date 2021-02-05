b7f4eb86a1be18cea1650b60a5d573516a1cc412
YangZimeng <yangzimeng17@gmail.com>
2021-02-05 14:22:30 -0500

    last changes

--- esc190_lab3.c
+++ esc190_lab3.c
@@ -2,75 +2,16 @@
 #include <ctype.h>
 
 
-char *my_strcat(char *dest, char *src);
-//int my_strcmp_rec(char *dest, char *src);
-int my_strcmp_rec(char *, char *);
 int atoi(char *str);
 
 
 int main(void)
 {
-    //char one[50] = "This is ";
-    //char two[] = "my house.";
-    //my_strcat(one, two);
-    //printf("%s\n", one);
-
-    int i = atoi("1234");
+    int i = atoi("-90");
     printf("%d\n", i);
-
-    /*
-    //Q2
-    char *one = "This is c";
-    char *two = "This is a";
-    int answer;
-    answer = my_strcmp_rec(one, two);
-    printf("%d\n", answer);
-    */
+    
     return 0;
 }
-
-
-char *my_strcat(char *dest, char *src){
-    char *ptr = dest;
-    char *ptr1 = src;
-    while (*ptr != '\0'){
-        ptr++;
-    }
-    while (*ptr1 != '\0'){
-        *ptr = *ptr1;
-        ptr++;
-        ptr1++;
-    }
-    *ptr1 = '\0';
-
-    return dest;
-}
-
-/*
-int my_strcmp_rec(char *dest, char *src){
-
-    if (*dest == *src)
-    {
-        return 0;
-    }
-    
-
-    if (*dest != *src)
-    {
-        return (*dest - *src);
-    }
-    else if (*dest =='\0')
-    {
-        return 0;
-    } 
-    else
-    {
-        return my_strcmp_rec(++dest, ++src);
-    }    
-
-}
-*/
-
 
 int atoi(char *str){
     int res=0;
