f46b72e7ba857c0396f99b04071077011d462742
YangZimeng <yangzimeng17@gmail.com>
2021-02-04 19:55:52 -0500

    finished q3

--- esc190_lab3.c
+++ esc190_lab3.c
@@ -40,24 +40,36 @@ char *my_strcat(char *dest, char *src){
 
 int my_strcmp_rec(char *dest, char *src){
 
+    if (*dest == *src)
+    {
+        return 0;
+    }
     
 }
 
 
 int atoi(char *str){
     // does not work yet
-    char *ptr = str;
-    int res = 0;
-    int count = 1;
-    while (*ptr != '\0'){
-        printf("%s\n", *ptr);
-        if (isdigit(*ptr)){
-            res = (*ptr-'0')*count + res;
-            count = count*10;
-        }
-        ptr++;
-    }
+    int res=0;
+    int multiplier = 1;
     
-    return res;
-  
+    if (*str == '-')
+    {
+        multiplier = -1;
+        str++;
+    }
+    else
+    {
+        multiplier = 1;
+    }
+    while (*str)
+    {
+        res *= 10;
+        res += *str - '0';
+
+        str++;
+    }
+    return(res * multiplier);
 }
+  
+
