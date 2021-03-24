312d70be08727782047f259a658dc865287a8295
YangZimeng <yangzimeng17@gmail.com>
2021-03-24 12:30:30 -0400

    set up

---
+++ lab7/lab7_q2.py
@@ -0,0 +1,23 @@
+'''
+Write Python code to find the cost of the minimum-energy seam in a list of lists.
+
+energies = [[24,      22,      30,      15,      18,      19],
+            [12,      23,      15,      23,      10,     15],
+            [11,      13,      22,      13,      21,      14],
+            [13,      15,      17,      28,      19,      21],
+            [17,      17,      7,       27,      20,      19]]
+     
+    The correct output for the given energies data is 15+10+13+17+7 = 62.
+
+'''
+def find_cost(L):
+    pass
+
+
+
+#to test:
+L = [[24,      22,      30,      15,      18,      19],
+    [12,      23,      15,      23,      10,     15],
+    [11,      13,      22,      13,      21,      14],
+    [13,      15,      17,      28,      19,      21],
+    [17,      17,      7,       27,      20,      19]]
