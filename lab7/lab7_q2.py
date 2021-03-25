968a68609ddf2aeda8093880038d6d86a4259ae9
YangZimeng <yangzimeng17@gmail.com>
2021-03-24 20:52:22 -0400

    finished q2

--- lab7/lab7_q2.py
+++ lab7/lab7_q2.py
@@ -11,7 +11,37 @@ energies = [[24,      22,      30,      15,      18,      19],
 
 '''
 def find_cost(L):
-    pass
+    res_L = []
+    min = L[0][0]
+    index = -1
+    for i in range(len(L[1])):
+        if L[0][i] < min:
+            min = L[0][i]
+            index = i
+    res_L.append(min)
+
+    for i in range(1, len(L)):
+        min = L[i][index - 1]
+        index_m = index
+        for j in range (index_m -1, index_m + 2):
+            #print(index + j)
+            #print(L[i][index + j])
+            if L[i][j] < min:
+                min = L[i][j]
+            index_m = L[i].index(min)
+            #print(index_m)
+        res_L.append(min)
+
+    print(res_L)
+    res = 0
+    for k in res_L:
+        res += k
+
+    return res
+                
+
+        
+
 
 
 
@@ -21,3 +51,6 @@ L = [[24,      22,      30,      15,      18,      19],
     [11,      13,      22,      13,      21,      14],
     [13,      15,      17,      28,      19,      21],
     [17,      17,      7,       27,      20,      19]]
+
+min_cost = find_cost(L)
+print(min_cost)
