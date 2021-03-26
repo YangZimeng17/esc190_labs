44a3a3cf7dda6b126668ee3bb0db0e15cac4eb0b
YangZimeng <yangzimeng17@gmail.com>
2021-03-26 15:03:21 -0400

    q2

--- lab7/lab7_q2.py
+++ lab7/lab7_q2.py
@@ -12,45 +12,42 @@ energies = [[24,      22,      30,      15,      18,      19],
 '''
 def find_cost(L):
     res_L = []
+    res = 0
     min = L[0][0]
     index = -1
-    for i in range(len(L[1])):
-        if L[0][i] < min:
-            min = L[0][i]
-            index = i
-    res_L.append(min)
-
-    for i in range(1, len(L)):
-        min = L[i][index - 1]
-        index_m = index
-        for j in range (index_m -1, index_m + 2):
-            #print(index + j)
-            #print(L[i][index + j])
-            if L[i][j] < min:
-                min = L[i][j]
-            index_m = L[i].index(min)
-            #print(index_m)
-        res_L.append(min)
-
-    print(res_L)
-    res = 0
-    for k in res_L:
+    for k in L[0]:
         res += k
+        index_m = L[0].index(k)
 
-    return res
-                
-
-        
-
-
-
+        for i in range(1, len(L)):
+            if index_m == 0:
+                min = L[i][0]
+            else:
+                min = L[i][index_m - 1]
+            for j in range (index_m -1, index_m + 2):
+                #print(index + j)
+                #print(L[i][index + j])
+                if j > len(L) - 1:
+                    break
+                if L[i][j] < min:
+                    min = L[i][j]
+                index_m = L[i].index(min)
+                #print(index_m)
+            res += min
+        res_L.append(res)
+    min = res_L[0]
+    for e in res_L:
+        if e < min:
+            min = e
+    
+    return min
 
 #to test:
 L = [[24,      22,      30,      15,      18,      19],
     [12,      23,      15,      23,      10,     15],
     [11,      13,      22,      13,      21,      14],
-    [13,      15,      17,      28,      19,      21],
-    [17,      17,      7,       27,      20,      19]]
+    [1,      15,      17,      28,      19,      21],
+    [1,      17,      7,       27,      20,      19]]
 
 min_cost = find_cost(L)
 print(min_cost)
